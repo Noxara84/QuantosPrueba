1e5d33b86f114d6bffb0c3e29a84e50b5fa2df08
Noxara84 <d-aaron-man@live.com.mx>
2020-09-02 23:08:31 -0500

    11:09

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 
 const routes: Routes = [
   { path: 'home', component: AppComponent },
-  { path: '',   redirectTo: '/home', pathMatch: 'full' }
+  { path: '/home',   redirectTo: '/home', pathMatch: 'full' }
 ];
 
 @NgModule({
